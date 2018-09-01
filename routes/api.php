d03988075ac1f579089e95913d131f1e5d29918a
testrose <saki.0150625@gmail.com>
2018-09-01 20:30:22 +0600

    Migration created for product and reviews

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// api products route
 Route::apiResource('products','ProductController');
 
-
+// api reviews route
 Route::group(['prefix' => 'products'], function () {
     Route::apiResource('/{product}/reviews', 'ReviewController');
 });
